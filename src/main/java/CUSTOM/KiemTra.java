ffd5d53a6d69dcbb898c551d9782158c14940583
daringbob <100821194+daringbob@users.noreply.github.com>
2023-04-19 10:23:23 +0700

    Add files via upload
    Thêm hàm kiểm tra chuỗi chỉ chứa chữ và số không có khoảng cách, kiểm tra họ và tên,kiểm tra mail,sđt

--- src/main/java/CUSTOM/KiemTra.java
+++ src/main/java/CUSTOM/KiemTra.java
@@ -7,40 +7,73 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class KiemTra {
-	public KiemTra() {}
-	public boolean KTSo(String in)
-	{
-		for(int i=0;i<in.length();i++)
-		{
-			if(in.charAt(i)<48||in.charAt(i)>57)
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean KTKhoangCach(String in)
-	{
-		for(int i=0;i<in.length();i++)
-		{
-			if(in.charAt(i)==' ')
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-	public boolean KTKyTuDacBiet(String in)
-	{
-		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-		Matcher m = p.matcher(in);
-		boolean b = m.find();
-		if(b)
-			return true;
-		else
-			return false;
-	}
-	public boolean isValid(String dateStr) {
+
+    public KiemTra() {
+    }
+
+    public boolean KTSo(String in) {
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) < 48 || in.charAt(i) > 57) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean KTKhoangCach(String in) {
+        for (int i = 0; i < in.length(); i++) {
+            if (in.charAt(i) == ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean KTKyTuDacBiet(String in) {
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(in);
+        boolean b = m.find();
+        if (b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean KTKyTuDacBietKhongKhoangCach(String input){
+         String regex = "^[a-zA-Z0-9]*$"; // matches alphanumeric characters only
+         return input.matches(regex) && !input.contains(" "); // checks for no spaces and only alphanumeric characters
+
+    }
+
+    public boolean KTHoVaTen(String input){
+        return input.matches("^[a-zA-Z\\s]*$");
+    }
+    
+    public boolean KTKhongChuaSo(String in) {
+        Pattern p = Pattern.compile("[^a-z ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(in);
+        boolean b = m.find();
+        if (b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public  boolean isValidPhone(String input) {
+        String regex = "^[0-9]{1,12}$";
+        return input.matches(regex);
+    }
+
+    public boolean isValidEmail(String email) {
+    String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(email);
+    return matcher.matches();
+}
+    
+    public boolean isValid(String dateStr) {
         DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         sdf.setLenient(false);
         try {
